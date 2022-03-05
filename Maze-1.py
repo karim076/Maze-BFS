9c3c8a21f7ce049ea924d4b5cca024d9c8e9ecd7
karim076 <59794989+karim076@users.noreply.github.com>
2022-03-05 17:24:49 +0100

    Volledige Functie van het algorithme in werking(Breath First Search))

--- Maze-1.py
+++ Maze-1.py
@@ -13,3 +13,92 @@ def createMaze():
     maze.append(["#","#", "#", "#", "#", "#", "#", "X", "#"])
 
     return maze
+
+ef printMaze(maze, path=""):#maze bekijken
+    for x, pos in enumerate(maze[0]):
+        if pos == "O":
+            start = x
+
+    i = start
+    j = 0
+    pos = set()
+    for move in path:
+        if move == "L":
+            i -= 1
+
+        elif move == "R":
+            i += 1
+
+        elif move == "U":
+            j -= 1
+
+        elif move == "D":
+            j += 1
+        pos.add((j, i))
+    
+    for j, row in enumerate(maze):
+        for i, col in enumerate(row):
+            if (j, i) in pos:
+                print("+ ", end="")
+            else:
+                print(col + " ", end="")
+        print()
+        
+
+
+def valid(maze, moves):# begin en verplaatsen
+    for x, pos in enumerate(maze[0]):
+        if pos == "O":
+            start = x
+
+    i = start
+    j = 0
+    for move in moves:
+        if move == "L":
+            i -= 1
+
+        elif move == "R":
+            i += 1
+
+        elif move == "U":
+            j -= 1
+
+        elif move == "D":
+            j += 1
+
+        if not(0 <= i < len(maze[0]) and 0 <= j < len(maze)):
+            return False
+        elif (maze[j][i] == "#"):
+            return False
+
+    return True
+
+
+def findEnd(maze, moves):# het einde zoeken
+    for x, pos in enumerate(maze[0]):
+        if pos == "O":
+            start = x
+
+    i = start
+    j = 0
+    count = 0
+    for move in moves:
+        if move == "L":
+            i -= 1
+            count += 1
+        elif move == "R":
+            i += 1
+            count += 1
+        elif move == "U":
+            j -= 1
+            count += 1
+        elif move == "D":
+            j += 1
+            count += 1
+    if maze[j][i] == "X":
+        print("Gevonden: " + moves)
+        printMaze(maze, moves)
+        print(F"Het heeft {count} moves gekost om het doolhof te oplossen.")
+        return True
+
+    return False
