7bc81c83462bc98861496b2a812ffeca1a4d2c8e
karim076 <59794989+karim076@users.noreply.github.com>
2022-03-05 17:19:23 +0100

    Het doolhof(Maze)

--- Maze-1.py
+++ Maze-1.py
@@ -1,1 +1,15 @@
 import queue #library nodig
+
+def createMaze():
+    maze = []# Dit het doolhof je kan het doolhof veranderen als je wilt.
+    maze.append(["#","#", "#", "#", "#", "O", "#", "#", "#"])
+    maze.append(["#"," ", " ", " ", " ", " ", " ", " ", "#"])
+    maze.append(["#"," ", "#", "#", " ", "#", "#", " ", "#"])
+    maze.append(["#"," ", "#", " ", " ", " ", "#", " ", "#"])
+    maze.append(["#"," ", "#", " ", "#", " ", "#", " ", "#"])
+    maze.append(["#"," ", "#", " ", " ", "#", "#", " ", "#"])
+    maze.append(["#"," ", "#", " ", "#", " ", "#", "#", "#"])
+    maze.append(["#"," ", " ", " ", " ", " ", " ", " ", "#"])
+    maze.append(["#","#", "#", "#", "#", "#", "#", "X", "#"])
+
+    return maze
